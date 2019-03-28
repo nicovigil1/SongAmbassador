a4d77fb3151898392bcf1bf6e938050a92982ab9
Nico <nicovigil1@gmail.com>
2019-03-28 09:57:08 -0600

    adds song search bar and fetch reqeust to musixmatch api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,34 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <div>
-        <input type="text" placeholder="Search Artists" id="search-bar"/>
-        <button onClick={this.props.catchSearch} type="submit">Search</button>
+      <div style={setFlex}>
+        <input type="text" placeholder="Search Artists" style={styleSearchBar} id='search-bar' spellCheck='false'/>
+        <button onClick={this.props.catchSearch} type="submit" style={styleSubmit}>Search</button>
       </div>
     )
   }
+}
+
+const setFlex = {
+  display: 'flex',
+  justifyContent: 'center',
+  paddingBottom: '5px',
+  paddingTop: '5px',
+  // background: '#E15554'
+}
+
+const styleSearchBar = {
+  padding: '5px', 
+  borderRadius: '10px',
+  outline: 'none'
+}
+
+const styleSubmit = {
+  marginLeft: '7px',
+  borderRadius: '7px',
+  paddingLeft: '3px',
+  paddingRight: '3px',
+  outline: 'none'
 }
 
 export default SearchBar
